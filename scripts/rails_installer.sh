207dbe4b6281f52b3ebc20d93649a9807d321fbf
CJ Buchmann <cjbuchmann@gmail.com>
2014-11-02 18:17:56 -0800

    Update rails_installer.sh

--- scripts/rails_installer.sh
+++ scripts/rails_installer.sh
@@ -5,6 +5,7 @@ sudo apt-get install -y libxml2-dev libxslt1-dev
 
 # ruby versioner
 echo "~~ installing RVM ~~"
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 # TODO - rvm sourcing does not seem to work here
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source ~/.rvm/scripts/rvm
